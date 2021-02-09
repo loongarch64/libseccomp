6892b117385349fecc066f09fba61afa2deac737
Xiaotian Wu <wuxiaotian@loongson.cn>
2021-02-09 15:39:13 +0800

    arch: Add 64-bit LoongArch support
    Signed-off-by: Xiaotian Wu <wuxiaotian@loongson.cn>

--- include/seccomp-syscalls.h
+++ include/seccomp-syscalls.h
@@ -277,6 +277,7 @@
 #define __PNR_riscv_flush_icache		-10243
 #define __PNR_memfd_secret			-10244
 #define __PNR_map_shadow_stack			-10245
+#define __PNR_fstat				-10246
 
 /*
  * libseccomp syscall definitions
